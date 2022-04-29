c6fc8403517eeb54fee16423a1476c19403f00d6
svgfm01 <filip.mustic@svgroup.hr>
2022-04-29 13:08:00 +0200

    Initial project commit

---
+++ src/test/java/guru/springframework/filippetclinic/FilipPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package guru.springframework.filippetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FilipPetClinicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
